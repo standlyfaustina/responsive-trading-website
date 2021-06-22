8cb5062014a2eadeae0765ee775aa5676abfa681
Mr Standly <standlyfaustina@gmail.com>
2021-06-22 16:44:49 +0100

    home & feature

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ $(document).ready(function(){
         $('.navbar').toggleClass('nav-toggle');
     });
 
+    $('#share').click(function(){
+        $(this).toggleClass('share-active')
+    });
+
     $(window).on('load scroll',function(){
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+
+        $('section').each(function(){
+            
+            let height = $(this).height();
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top -200;
+
+            if(top >= offset && top < height + offset){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
     });
 });
